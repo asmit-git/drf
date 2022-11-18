3ff1feb47621c33cfe0a2cd407629022d97eb9d8
Asmit Gautam <mailtoasmitgautam@gmail.com>
2022-11-18 16:40:40 +0545

    viewsets and routers

--- backend/products/viewsets.py
+++ backend/products/viewsets.py
@@ -1,6 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import mixins, viewsets
 from .models import Product
 from .serializers import ProductSerializer
+
 
 class ProductViewSet(viewsets.ModelViewSet):
     '''
@@ -14,3 +15,23 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_field = 'pk'
+
+
+class ProductGenericViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet):
+    '''
+    get -> list all 
+    get -> retrieve detail
+    '''
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+'''
+product_list_create_view = ProductListCreateAPIView.as_view() similar as in views
+'''
+product_list_view = ProductGenericViewSet.as_view({'get':'list'})
+
+product_detail_view = ProductGenericViewSet.as_view({'get':'retrieve'})
